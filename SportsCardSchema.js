f374a42fe8ded34c3c31e85a2a8b7182409aa5c6
Halee Corbin <kuroyukihime@ufl.edu>
2019-02-18 18:37:11 -0500

    Started working on schema

---
+++ SportsCardSchema.js
@@ -0,0 +1,24 @@
+/* Import mongoose and define any variables needed to create the schema */
+/* This is only a work in progress and will likely change as the project starts moving along more */
+/* I assume we are going to use mongoose like we learned from the bootcamp assignments */
+
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var sportsCardSchema = new Schema (
+{
+    //imgFront: { data: Buffer, contentType: String }, //Placeholder, not functional yet. Not sure yet how we will handle images.
+    //imgBack: { data: Buffer, contentType: String },
+    playerName: String,
+    year: Date,
+    manufacturer: String,
+    cardNum: Number,
+    team: String,
+    //collectionInfo: //"any special collection information (tags, [strings])" What data type is this? String array?
+    otherInfo: String,
+    sold: Boolean
+});
+
+var Card = mongoose.model('Card', sportsCardSchema);
+
+module.exports = Card;
