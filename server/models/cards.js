7a0b0beb5c3f0c83bdd8b015fac27010fd691ca4
Halee Corbin <kuroyukihime@ufl.edu>
2019-02-22 18:34:45 -0500

    Removed old comments

--- server/models/cards.js
+++ server/models/cards.js
@@ -1,19 +1,16 @@
-/* This is only a work in progress and will likely change as the project starts moving along more */
-/* I assume we are going to use mongoose like we learned from the bootcamp assignments */
-
 var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var sportsCardSchema = new Schema (
 {
-    imgFront: String, //image path url
+    imgFront: String, //image path url, not sure yet how images will be handled.
     imgBack: String, //image path url
     playerName: String,
     year: Date,
     manufacturer: String,
     cardNum: Number,
     team: String,
-    collectionInfo: [String], //Haven't tested this yet
+    collectionInfo: [String],
     otherInfo: String,
     quantity: Number,
     sold: Boolean,
