059af17703320e58f51859dbc48ebb7115553330
Marco Austria <maustria0507@gmail.com>
2019-03-25 19:09:35 -0400

    added admin upload and admin catalog

--- client/js/app.js
+++ client/js/app.js
@@ -1,23 +1,28 @@
-angular.module('sportsCardApp',["ngRoute"])
+angular.module('sportsCardApp', ["ngRoute"])
 
-.config(function($routeProvider){
-  $routeProvider
-  .when("/",{
-      templateUrl: "about.html",
-      
-  })
-  .when("/catalog",{
-      templateUrl: "catalog.html",
-      controller: 'cardController'
-     
-  })
-  .otherwise({
-      templateUrl: "about.html"
-  });
-});
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when("/", {
+                templateUrl: "about.html",
 
+            })
+            .when("/catalog", {
+                templateUrl: "catalog.html",
+                controller: 'cardController'
 
+            })
+            .when("/adminupload", {
+                templateUrl: "adminupload.html",
+                //                controller: ;
 
+            })
+            .when("/admincatalog", {
+                templateUrl: "admincatalog.html",
+                //                controller: ;
 
+            })
 
-
+            .otherwise({
+                templateUrl: "about.html"
+            });
+    });
