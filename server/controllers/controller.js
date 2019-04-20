56d2241ffdde9dfbd4ba9d31b6611ad0769f529a
Halee Corbin <kuroyukihime@ufl.edu>
2019-04-20 01:10:50 -0400

    Uncomment for heroku

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -8,8 +8,8 @@ const {
 } = require('@google-cloud/storage');
 const storage = new Storage({
     projectId: 'sports-cards-test',
-    //credentials: JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS) //Uncomment this for heroku, comment out for localhost
-    keyFilename: './server/config/Sports-Cards-Test-2c43a0d8f8ab.json' //Uncomment this for localhost, comment out for heroku
+    credentials: JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS) //Uncomment this for heroku, comment out for localhost
+    //keyFilename: './server/config/Sports-Cards-Test-2c43a0d8f8ab.json' //Uncomment this for localhost, comment out for heroku
 });
 
 
